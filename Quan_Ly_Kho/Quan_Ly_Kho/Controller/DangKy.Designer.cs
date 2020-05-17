4257483d76a26b81e166679c676954ebf5ee60b9
unknown <khanhpino1206@gmail.com>
2020-05-17 16:51:28 +0700

    database , formdky , formDn

---
+++ Quan_Ly_Kho/Quan_Ly_Kho/Controller/DangKy.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace Quan_Ly_Kho.Controller
+{
+    partial class DangKy
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.txtPass = new System.Windows.Forms.TextBox();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackgroundImage = global::Quan_Ly_Kho.Properties.Resources.Đăng_ký;
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.txtPass);
+            this.panel1.Controls.Add(this.txtID);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(498, 297);
+            this.panel1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Quan_Ly_Kho.Properties.Resources._1_Đăng_ký;
+            this.pictureBox1.Location = new System.Drawing.Point(209, 228);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(131, 37);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // txtPass
+            // 
+            this.txtPass.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPass.Location = new System.Drawing.Point(181, 166);
+            this.txtPass.Multiline = true;
+            this.txtPass.Name = "txtPass";
+            this.txtPass.Size = new System.Drawing.Size(281, 35);
+            this.txtPass.TabIndex = 3;
+            // 
+            // txtID
+            // 
+            this.txtID.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtID.Location = new System.Drawing.Point(181, 106);
+            this.txtID.Multiline = true;
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(281, 35);
+            this.txtID.TabIndex = 2;
+            // 
+            // DangKy
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(498, 297);
+            this.Controls.Add(this.panel1);
+            this.Name = "DangKy";
+            this.Text = "DangKy";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox txtPass;
+        private System.Windows.Forms.TextBox txtID;
+    }
+}
---
+++ Quan_Ly_Kho/Quan_Ly_Kho/Controller/DangNhap.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Quan_Ly_Kho.Model;
+
+namespace Quan_Ly_Kho.Controller
+{
+    public partial class DangNhap : Form
+    {
+        Quan_Ly_KhoEntities db = new Quan_Ly_KhoEntities();
+        public DangNhap()
+        {
+            InitializeComponent();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void pBDangNhap_Click(object sender, EventArgs e)
+        {
+            string name = txtID.Text;
+            string pas = txtPass.Text;
+            if (name.Length == 0) MessageBox.Show("Bạn Chưa điền tên tài khoản", "thông báo");
+            TaiKhoan taikhoan1 = db.TaiKhoans.SingleOrDefault(x => x.id == name && x.pass == pas);
+            if (taikhoan1 != null)
+            {
+              
+            }
+            else MessageBox.Show("Tài khoản hoặc mật khẩu không chính xác!", "thông báo");
+        }
+
+        private void pBDangKy_Click(object sender, EventArgs e)
+        {
+            DangKy DK = new DangKy();
+            this.Hide();
+            DK.ShowDialog();
+            this.Close();
+        }
+    }
+}
